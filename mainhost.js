b34a496408846b750d5f6157f4c7147f632ae24d
MaVuong <hobach@HoBachs-Mac-mini.local>
2016-07-11 10:50:39 +0700

    kk
    cvv

--- mainhost.js
+++ mainhost.js
@@ -6,7 +6,7 @@ var http = require('http').Server(app);
 
 app.use(express.static(__dirname + '/mainhost'));
 
-
+//sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 2500
 
 http.listen(2500, function(){
 	console.log('listening on : 2500');

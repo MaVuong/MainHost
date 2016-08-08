c1bc58074e0bfadd4d1fd9cfb99b7469ced7bd0e
MaVuong <hobach@HoBachs-iMac.local>
2016-08-08 23:27:56 +0700

    new
    sss

--- mainhost.js
+++ mainhost.js
@@ -7,6 +7,7 @@ var http = require('http').Server(app);
 app.use(express.static(__dirname + '/mainhost'));
 
 //sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 2500
+//sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 2500
 
 http.listen(2500, function(){
 	console.log('listening on : 2500');
